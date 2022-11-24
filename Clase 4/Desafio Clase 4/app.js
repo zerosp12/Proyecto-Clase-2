41c2a33bfea12802e02ed8826260b2d1bc0eb469
Zero <zero.sp12@gmail.com>
2022-11-23 23:13:36 -0300

    modified:   Clase 4/Desafio Clase 4/app.js 	modified:   Clase 4/Desafio Clase 4/index.html

--- Clase 4/Desafio Clase 4/app.js
+++ Clase 4/Desafio Clase 4/app.js
@@ -3,17 +3,47 @@ const app = new Vue({
 
     data: {
 
+        //-----------------------------
+        // Clientes
+        //-----------------------------
+        estiloClientes: {
+            'table': true, 
+            'table-hover': true,
+            'table-bordered': true,
+            'table-light': true,
+        },
+
         listaClientes: [
             { name: 'Juan', name2: 'Perez' },
             { name: 'Ricardo', name2: 'Barin' },
             { name: 'Bruno', name2: 'Diaz' },
         ],
 
+        //-----------------------------
+        // Vehiculos
+        //-----------------------------
+        estiloVehiculos: {
+            'table': true, 
+            'table-hover': true,
+            'table-bordered': true,
+            'table-info': true,
+        },
+
         listaVehiculos: [
             { name: 'Ford Fiesta', name2: '2010' },
             { name: 'Renault Oroch', name2: '2018' },
             { name: 'Volkswagen Passat', name2: '2014' },
         ],
+
+        //-----------------------------
+        // Sucursales
+        //-----------------------------
+        estiloSucursales: {
+            'table': true, 
+            'table-hover': true,
+            'table-bordered': true,
+            'table-dark': true,
+        },
 
         listaSucursales: [
             { name: 'Sucursal Central', name2: 'Fake Street 123' },
