67822baa9da5ebf6aae45d3e157dde683ce84c17
Zero <zero.sp12@gmail.com>
2022-11-30 22:28:42 -0300

    new file:   desafio-clase-6/.gitignore 	new file:   desafio-clase-6/README.md 	new file:   desafio-clase-6/babel.config.js 	new file:   desafio-clase-6/jsconfig.json 	new file:   desafio-clase-6/package-lock.json 	new file:   desafio-clase-6/package.json 	new file:   desafio-clase-6/public/favicon.ico 	new file:   desafio-clase-6/public/index.html 	new file:   desafio-clase-6/src/App.vue 	new file:   desafio-clase-6/src/assets/logo.png 	new file:   desafio-clase-6/src/components/RenderTable.vue 	new file:   desafio-clase-6/src/main.js 	new file:   desafio-clase-6/vue.config.js

---
+++ desafio-clase-6/src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="app">
+    <div class="container p-4 mt-4">
+      <h3>Lista de Clientes</h3>
+      <render-table
+        :objetos="listaClientes"
+        :estilo="estiloClientes"
+        tipo="0"
+      />
+      <h3>Lista de Vehículos</h3>
+      <render-table
+        :objetos="listaVehiculos"
+        :estilo="estiloVehiculos"
+        tipo="1"
+      />
+      <h3>Lista de Sucursales</h3>
+      <render-table
+        :objetos="listaSucursales"
+        :estilo="estiloSucursales"
+        tipo="2"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import RenderTable from "./components/RenderTable.vue";
+
+export default {
+  name: "App",
+  components: {
+    RenderTable,
+  },
+  data() {
+    return {
+      //-----------------------------
+      // Clientes
+      //-----------------------------
+      estiloClientes: {
+        table: true,
+        "table-hover": true,
+        "table-bordered": true,
+        "table-light": true,
+      },
+
+      listaClientes: [
+        { name: "Juan", name2: "Perez" },
+        { name: "Ricardo", name2: "Barin" },
+        { name: "Bruno", name2: "Diaz" },
+      ],
+
+      //-----------------------------
+      // Vehiculos
+      //-----------------------------
+      estiloVehiculos: {
+        table: true,
+        "table-hover": true,
+        "table-bordered": true,
+        "table-info": true,
+      },
+
+      listaVehiculos: [
+        { name: "Ford Fiesta", name2: "2010" },
+        { name: "Renault Oroch", name2: "2018" },
+        { name: "Volkswagen Passat", name2: "2014" },
+      ],
+
+      //-----------------------------
+      // Sucursales
+      //-----------------------------
+      estiloSucursales: {
+        table: true,
+        "table-hover": true,
+        "table-bordered": true,
+        "table-dark": true,
+      },
+
+      listaSucursales: [
+        { name: "Sucursal Central", name2: "Fake Street 123" },
+        { name: "Sucursal CABA", name2: "Balcarce 50" },
+        { name: "Sucursal Nueva", name2: "Alguna Calle 1234" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ desafio-clase-6/src/components/RenderTable.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="table-responsive text-center">
+    <table :class="estilo">
+      <thead>
+        <tr>
+          <th scope="col" class="w-50">{{ nombresColumnas[tipo][0] }}</th>
+          <th scope="col">{{ nombresColumnas[tipo][1] }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="" v-for="(objeto, index) of objetos" :key="index">
+          <td scope="row">{{ objeto.name }}</td>
+          <td>{{ objeto.name2 }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RenderTable",
+
+  data: function () {
+    return {
+      nombresColumnas: [
+        ["Nombre", "Apellido"],
+        ["Marca", "Modelo"],
+        ["Sucursal", "Calle"],
+      ],
+    };
+  },
+
+  props: {
+    estilo: String,
+    tipo: Number,
+    objetos: Object,
+  },
+};
+</script>
+
+<style scoped>
+</style>
